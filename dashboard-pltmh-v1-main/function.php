4812883a7f48962b7cd326db5547716e55fb5122
Wahyu Exano <exano.alcordova@gmail.com>
2023-08-23 11:30:40 +0700

    Update function.php
    ip local

--- dashboard-pltmh-v1-main/function.php
+++ dashboard-pltmh-v1-main/function.php
@@ -12,7 +12,7 @@
 //         $koneksi2 = mysqli_connect("localhost", "root", "", "pltmh_bandung");
 //     }
 // }
-$koneksi = mysqli_connect("128.199.118.14", "komatsu", "komatsu123456789", "pltmh_kemuning");
+$koneksi = mysqli_connect("127.0.0.1", "komatsu", "komatsu12345XXX", "pltmh_kemuning");
 function query($query) 
 {
     global $koneksi;
